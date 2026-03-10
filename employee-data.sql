5af74c3c2c89cb68d04795ef0e1af60730108c38
sbtalk71 <sbtalk@yahoo.com>
2026-03-10 11:28:47 +0530

    Create employee-data.sql

---
+++ employee-data.sql
@@ -0,0 +1,23 @@
+
+create table Employee (EMPNO integer 
+not null, NAME varchar(15),
+SALARY double,
+ADDRESS varchar(15),
+DNO integer, primary key (EMPNO));
+
+
+
+
+insert into Employee (EMPNO,NAME,SALARY,ADDRESS,DNO) values (100,'Shantanu',30000,'Hyd',10);
+insert into Employee (EMPNO,NAME,SALARY,ADDRESS,DNO) values (101,'Rajiv',30000,'Hyd',10);
+insert into Employee (EMPNO,NAME,SALARY,ADDRESS,DNO) values (102,'Shankar',30000,'Blore',20);
+insert into Employee (EMPNO,NAME,SALARY,ADDRESS,DNO) values (103,'Madhu',30000,'Hyd',20);
+insert into Employee (EMPNO,NAME,SALARY,ADDRESS,DNO) values (104,'Kannan',30000,'Dhanbad',20);
+insert into Employee (EMPNO,NAME,SALARY,ADDRESS,DNO) values (105,'Vimal',30000,'Hyd',10);
+insert into Employee (EMPNO,NAME,SALARY,ADDRESS,DNO) values (106,'Rupa',30000,'Bhopal',30);
+insert into Employee (EMPNO,NAME,SALARY,ADDRESS,DNO) values (107,'Pavan',30000,'Hyd',30);
+insert into Employee (EMPNO,NAME,SALARY,ADDRESS,DNO) values (108,'Kalyan',30000,'Chennai',30);
+insert into Employee (EMPNO,NAME,SALARY,ADDRESS,DNO) values (109,'Bhavesh',30000,'Hyd',20);
+insert into Employee (EMPNO,NAME,SALARY,ADDRESS,DNO) values (110,'Arun',30000,'Hyd',20);
+insert into Employee (EMPNO,NAME,SALARY,ADDRESS,DNO) values (111,'Kanti',30000,'Hyd',10);
+insert into Employee (EMPNO,NAME,SALARY,ADDRESS,DNO) values (112,'Suraj',30000,'Hyd',10);
