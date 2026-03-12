424c3625294342b2209d0e591818bf621bc20c32
sbtalk71 <sbtalk@yahoo.com>
2026-03-12 17:04:25 +0530

    slides updated

--- apps/jdbc-apps/src/com/demo/jdbc/JdbcApp.java
+++ apps/jdbc-apps/src/com/demo/jdbc/JdbcApp.java
@@ -8,11 +8,11 @@ import java.sql.Statement;
 public class JdbcApp {
     public static void main(String[] args) throws Exception {
         //load the driver
-        Class.forName("org.mariadb.jdbc.Driver");
+        //Class.forName("org.mariadb.jdbc.Driver");
 
         //get the connection
-        Connection conn= DriverManager.getConnection("jdbc:mariadb://localhost:3307/training","root","root");
-
+       // Connection conn= DriverManager.getConnection("jdbc:mariadb://localhost:3307/training","root","root");
+Connection conn=DriverManager.getConnection("jdbc:postgresql://trainingserver.postgres.database.azure.com:5432/postgres","training","welcome@123");
         if(conn!=null){
             System.out.println("Mariadb Connected.....");
             System.out.println(conn.getClass().getName());
